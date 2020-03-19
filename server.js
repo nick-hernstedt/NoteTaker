0ecd41c6a02a300301365b8e0271852d9781d5db
copiousamounts <nick.hernstedt@gmail.com>
2020-03-19 15:58:07 -0700

    added routes to the style page and index page

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ var express = require("express");
 var path = require("path");
 const fs = require(`fs`);
 const notes = require(`./db/db.json`);
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
