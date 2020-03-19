7c33f246aeaa05b5deb0aaf1b0f46d96864a2fa5
copiousamounts <nick.hernstedt@gmail.com>
2020-03-19 16:02:56 -0700

    added routes to index.js and notes.html

--- server.js
+++ server.js
@@ -13,3 +13,22 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
+
+// Routes
+
+// Basic route that sends the user first tot he AJAX page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("public/assets/css/stlyes.css", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/assets/css/stlyes.css"));
+});
+
+app.get("/public/assets/js/index.js", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/assets/js/index.js"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
